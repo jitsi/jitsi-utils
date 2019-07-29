aca0e201b4f88954df554f16224fce024295504e
Boris Grozev <boris@jitsi.org>
2019-07-29 13:58:53 +0100

    Adds VersionImpl
    Replaces AbstractVersion with a complete implementation.

--- src/main/java/org/jitsi/utils/version/VersionImpl.java
+++ src/main/java/org/jitsi/utils/version/VersionImpl.java
@@ -16,29 +16,38 @@
 package org.jitsi.utils.version;
 
 /**
- * Base class for <tt>Version</tt> implementation that uses major, minor and
- * nightly build id for versioning purposes.
+ * An implementation of {@link Version}.
  *
  * @author Emil Ivov
  * @author Pawel Domas
+ * @author Boris Grozev
  */
-public abstract class AbstractVersion
-    implements Version
+public class VersionImpl implements Version
 {
+    /**
+     * The application name field.
+     */
+    private final String applicationName;
+
     /**
      * The version major field.
      */
-    private int versionMajor;
+    private final int versionMajor;
 
     /**
      * The version minor field.
      */
-    private int versionMinor;
+    private final int versionMinor;
 
     /**
      * The nightly build id field.
      */
-    private String nightlyBuildID;
+    private final String nightlyBuildID;
+
+    /**
+     * The pre-release ID field.
+     */
+    private final String preReleaseId;
 
     /**
      * Creates version object with custom major, minor and nightly build id.
@@ -47,49 +56,80 @@ public abstract class AbstractVersion
      * @param minorVersion the minor version to use.
      * @param nightlyBuildID the nightly build id value for new version object.
      */
-    protected AbstractVersion(int majorVersion,
-                              int minorVersion,
-                              String nightlyBuildID)
+    public VersionImpl(String applicationName,
+                          int majorVersion,
+                          int minorVersion)
     {
-        this.versionMajor = majorVersion;
-        this.versionMinor = minorVersion;
-        this.nightlyBuildID = nightlyBuildID;
+        this(applicationName, majorVersion, minorVersion, null);
     }
 
     /**
-     * Returns the version major of the current Jitsi version. In an
-     * example 2.3.1 version string 2 is the version major. The version major
-     * number changes when a relatively extensive set of new features and
-     * possibly rearchitecturing have been applied to the Jitsi.
+     * Creates version object with custom major, minor and nightly build id.
      *
-     * @return the version major String.
+     * @param majorVersion the major version to use.
+     * @param minorVersion the minor version to use.
+     * @param nightlyBuildID the nightly build id value for new version object.
      */
+    public VersionImpl(String applicationName,
+                          int majorVersion,
+                          int minorVersion,
+                          String nightlyBuildID)
+    {
+        this(applicationName, majorVersion, minorVersion, nightlyBuildID, null);
+    }
+
+    /**
+     * Creates version object with custom major, minor and nightly build id.
+     *
+     * @param majorVersion the major version to use.
+     * @param minorVersion the minor version to use.
+     * @param nightlyBuildID the nightly build id value for new version object.
+     */
+    public VersionImpl(String applicationName,
+                          int majorVersion,
+                          int minorVersion,
+                          String nightlyBuildID,
+                          String preReleaseId)
+    {
+        this.applicationName = applicationName;
+        this.versionMajor = majorVersion;
+        this.versionMinor = minorVersion;
+        this.nightlyBuildID = nightlyBuildID;
+        this.preReleaseId = preReleaseId;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public int getVersionMajor()
     {
         return versionMajor;
     }
 
     /**
-     * Returns the version minor of the current Jitsi version. In an
-     * example 2.3.1 version string 3 is the version minor. The version minor
-     * number changes after adding enhancements and possibly new features to a
-     * given Jitsi version.
-     *
-     * @return the version minor integer.
+     * {@inheritDoc}
      */
+    @Override
     public int getVersionMinor()
     {
         return versionMinor;
     }
 
     /**
-     * If this is a nightly build, returns the build identifies (e.g.
-     * nightly-2007.12.07-06.45.17). If this is not a nightly build Jitsi
-     * version, the method returns null.
-     *
-     * @return a String containing a nightly build identifier or null if this is
-     * a release version and therefore not a nightly build
+     * {@inheritDoc}
      */
+    @Override
+    public boolean isNightly()
+    {
+        return nightlyBuildID != null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public String getNightlyBuildID()
     {
         if(!isNightly())
@@ -99,17 +139,25 @@ public abstract class AbstractVersion
     }
 
     /**
-     * Compares another <tt>Version</tt> object to this one and returns a
-     * negative, zero or a positive integer if this version instance represents
-     * respectively an earlier, same, or later version as the one indicated
-     * by the <tt>version</tt> parameter.
-     *
-     * @param version the <tt>Version</tt> instance that we'd like to compare
-     * to this one.
-     *
-     * @return a negative integer, zero, or a positive integer as this object
-     * represents a version that is earlier, same, or more recent than the one
-     * referenced by the <tt>version</tt> parameter.
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isPreRelease()
+    {
+        return preReleaseId != null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getPreReleaseID()
+    {
+        return preReleaseId;
+    }
+
+    /**
+     * {@inheritDoc}
      */
     public int compareTo(Version version)
     {
@@ -173,14 +221,7 @@ public abstract class AbstractVersion
     }
 
     /**
-     * Compares the <tt>version</tt> parameter to this version and returns true
-     * if and only if both reference the same Jitsi version and
-     * false otherwise.
-     *
-     * @param version the version instance that we'd like to compare with this
-     * one.
-     * @return true if and only the version param references the same
-     * Jitsi version as this Version instance and false otherwise.
+     * {@inheritDoc}
      */
     @Override
     public boolean equals(Object version)
@@ -194,6 +235,12 @@ public abstract class AbstractVersion
         return toString().equals(version.toString());
     }
 
+    @Override
+    public String getApplicationName()
+    {
+        return applicationName;
+    }
+
     /**
      * @see java.lang.Object#hashCode()
      */
@@ -204,9 +251,7 @@ public abstract class AbstractVersion
 
     /**
      * Returns a String representation of this Version instance in the generic
-     * form of major.minor[.nightly.build.id]. If you'd just like to obtain the
-     * version of Jitsi so that you could display it (e.g. in a Help->About
-     * dialog) then all you need is calling this method.
+     * form of major.minor[.nightly.build.id].
      *
      * @return a major.minor[.build] String containing the complete
      * Jitsi version.
