4e7a2946b2d0d3759e1b0401dd96414697a1334b
Brian Baldino <bbaldino@gmail.com>
2019-10-07 12:49:09 -0700

    config utils

---
+++ src/test/java/org/jitsi/utils/config/ReadOncePropertyTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.utils.config;
+
+import org.jitsi.utils.collections.*;
+import org.junit.*;
+
+import java.util.concurrent.atomic.*;
+import java.util.function.*;
+
+import static org.junit.Assert.*;
+
+public class ReadOncePropertyTest
+{
+    @Test
+    public void readsEveryTime()
+    {
+        AtomicInteger numTimesGetterCalled = new AtomicInteger(0);
+        AnswerProp answerProp = new AnswerProp(() -> {
+            numTimesGetterCalled.incrementAndGet();
+            return 42;
+        });
+
+        answerProp.get();
+        answerProp.get();
+        answerProp.get();
+        answerProp.get();
+
+        assertEquals(1, numTimesGetterCalled.get());
+    }
+
+    protected static class AnswerProp extends ReadOnceProperty<Integer>
+    {
+        AnswerProp(Supplier<Integer> getter)
+        {
+            super(JList.of(getter));
+        }
+    }
+}
