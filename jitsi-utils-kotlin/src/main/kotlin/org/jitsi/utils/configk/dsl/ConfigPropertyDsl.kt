b31ba4f3bab71ef52c23d295f10da41749692893
Brian Baldino <bbaldino@gmail.com>
2019-11-07 17:19:31 -0800

    more dsl experimentation

--- jitsi-utils-kotlin/src/main/kotlin/org/jitsi/utils/configk/dsl/ConfigPropertyDsl.kt
+++ jitsi-utils-kotlin/src/main/kotlin/org/jitsi/utils/configk/dsl/ConfigPropertyDsl.kt
@@ -19,40 +19,116 @@ package org.jitsi.utils.configk.dsl
 import org.jitsi.utils.configk.ConfigProperty
 import org.jitsi.utils.configk.ConfigPropertyAttributes
 import org.jitsi.utils.configk.ConfigResult
-import org.jitsi.utils.configk.PropertyDelegate
-import org.jitsi.utils.configk.PropertyDelegateImpl
+import org.jitsi.utils.configk.FallbackConfigProperty
+import org.jitsi.utils.configk.PropValueRetriever
 import org.jitsi.utils.configk.getOrThrow
 import org.jitsi.utils.configk.readOnce
 import org.jitsi.utils.configk.spi.TypedConfigValueGetterService
+import java.time.Duration
 import kotlin.reflect.KClass
+
+
+//NOTE: as of now, this file is a playground until we decide on something
 
 fun legacyConfig() = Any()
 fun newConfig() = Any()
 
-class XXX : ConfigProperty<Boolean> by property({
+class XXX : ConfigProperty<Long> by property({
     attributes {
         readOnce()
     }
     search(
-        "legacy".asType<Int>() from legacyConfig(),
-        //"blah" from legacyConfig() asType<Int>().convertedTo<Long> {}
-        ("new".asType<Long>() from newConfig()).convertedTo { it.toInt() }
+        "legacyPropName" from legacyConfig() asType(Long::class),
+        "newPropName" from newConfig() asType(Duration::class) convertedBy { it.toMillis() }
     )
 })
+
+abstract class DefaultConfig<T : Any>(
+    clazz: KClass<T>,
+    attributes: ConfigPropertyAttributes,
+    legacyPropName: String,
+    newPropName: String
+) : FallbackConfigProperty<T>() {
+
+    private val legacyProp = PropSource(
+        clazz,
+        attributes = attributes,
+        path = legacyPropName,
+        configSource = legacyConfig()
+    )
+    private val newProp = PropSource(
+        clazz,
+        attributes = attributes,
+        path = newPropName,
+        configSource = newConfig()
+    )
+
+    override val propertyPriority: List<ConfigResult<T>>
+        get() = listOf(legacyProp.result, newProp.result)
+}
+
+class Simple : DefaultConfig<Int>(Int::class, readOnce(), legacyPropName, newPropName) {
+    companion object {
+        const val legacyPropName = "legacyName"
+        const val newPropName = "newName"
+    }
+}
+
+class YYY : ConfigProperty<Long> {
+    private val attributes = ConfigPropertyAttributes(readOnce = true)
+    val legacyProp = createProp<Long>(
+        attributes = attributes,
+        propName = "legacyName",
+        configSource = legacyConfig()
+    )
+    val newProp = createProp<Long>(
+        attributes = attributes,
+        propName = "newName",
+        configSource = newConfig()
+    )
+
+    override val value: Long
+        get() = TODO() //legacyProp.result.getOrElse { newProp.result.getOrThrow() }
+}
+
+inline fun <reified T : Any> createProp(
+    attributes: ConfigPropertyAttributes,
+    propName: String,
+    configSource: Any
+) : PropSource<T> = PropSource(T::class, attributes, propName, configSource)
+
+inline fun <reified T : Any> ConfigPropertyAttributes.propSource(name: String, configSource: Any) =
+    PropSource(T::class, this, name, configSource)
+
+class PropSource<T : Any>(
+    clazz: KClass<T>,
+    val attributes: ConfigPropertyAttributes,
+    val path: String,
+    val configSource: Any
+) {
+    private val propValueRetriever: PropValueRetriever<T>
+    init {
+        val typedGetter = TypedConfigValueGetterService.getterFor(clazz)
+        val supplier = { typedGetter(configSource, path) }
+        propValueRetriever = PropValueRetriever(attributes, supplier)
+    }
+
+    val result: ConfigResult<T>
+        get() = propValueRetriever.result
+}
 
 data class PropBuilder(
     var attributes: ConfigPropertyAttributes? = null
 ) {
-    inner class PropNameType<T : Any>(val name: String, val clazz: KClass<T>)
+    inner class PropNameSource(val name: String, val cofigSource: Any)
 
-    inline fun <reified T : Any> String.asType(): PropBuilder.PropNameType<T> =
-        this@PropBuilder.PropNameType(this, T::class)
-
-    inline infix fun <reified T : Any> PropBuilder.PropNameType<T>.from(configInstance: Any): PropertyDelegate<T> {
-        val typedGetter = TypedConfigValueGetterService.getterFor(clazz)
-        val supplier = { typedGetter(configInstance, this.name) }
-        return PropertyDelegateImpl(this@PropBuilder.attributes!!, supplier)
+    inline infix fun <reified T : Any> PropNameSource.asType(clazz: KClass<T>): PropValueRetriever<T> {
+        val typedGetter = TypedConfigValueGetterService.getterFor(T::class)
+        val supplier = { typedGetter(this.cofigSource, this.name) }
+        return PropValueRetriever<T>(this@PropBuilder.attributes!!, supplier)
     }
+
+    infix fun String.from(configSource: Any): PropNameSource = PropNameSource(this, configSource)
 }
 
 fun PropBuilder.attributes(block: ConfigPropertyAttributes.() -> Unit) {
@@ -60,16 +136,16 @@ fun PropBuilder.attributes(block: ConfigPropertyAttributes.() -> Unit) {
     attrs.block()
 }
 
-fun <T : Any, U : Any> PropertyDelegate<T>.convertedTo(block: (T) -> U): PropertyDelegate<U> {
-    return propDelegateHelper(this.attributes) { block(getValue().getOrThrow())}
+infix fun <T : Any, U : Any> PropValueRetriever<T>.convertedBy(block: (T) -> U): PropValueRetriever<U> {
+    return propValueRetrieverHelper(this.attributes) { block(result.getOrThrow())}
 }
 
-fun<U : Any> propDelegateHelper(attrs: ConfigPropertyAttributes, supplier: () -> U): PropertyDelegateImpl<U> {
-    return object : PropertyDelegateImpl<U>(attrs, supplier) {}
+fun<U : Any> propValueRetrieverHelper(attrs: ConfigPropertyAttributes, supplier: () -> U): PropValueRetriever<U> {
+    return PropValueRetriever(attrs, supplier)
 }
 
-inline fun <reified T : Any> PropBuilder.search(vararg configProps: PropertyDelegate<T>): List<PropertyDelegate<T>> =
-    configProps.toList()
+inline fun <reified T : Any> PropBuilder.search(vararg retrievers: PropValueRetriever<T>): List<PropValueRetriever<T>> =
+    retrievers.toList()
 
 inline fun <reified T : Any> property(block: PropBuilder.() -> Unit): ConfigProperty<T> {
     with (PropBuilder()) {
