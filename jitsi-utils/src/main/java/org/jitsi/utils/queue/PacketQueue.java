19967f333ce4eec93159935ab2ca726fbd3e984d
Jonathan Lennox <jonathan.lennox@8x8.com>
2020-02-04 15:30:54 -0500

    Copy javadoc to PacketQueue interface.

--- jitsi-utils/src/main/java/org/jitsi/utils/queue/PacketQueue.java
+++ jitsi-utils/src/main/java/org/jitsi/utils/queue/PacketQueue.java
@@ -7,16 +7,48 @@ import java.util.concurrent.*;
 
 public interface PacketQueue<T>
 {
+    /**
+     * Adds a specific packet ({@code T}) instance to the queue.
+     * @param pkt the packet to add.
+     */
     void add(T pkt);
 
+    /**
+     * Removes and returns the packet ({@code T}) at the head of this queue.
+     * Blocks until there is a packet in the queue. Returns {@code null} if
+     * the queue is closed or gets closed while waiting for a packet to be added.
+     * @return the packet at the head of this queue.
+     */
     T get();
 
+    /**
+     * Removes and returns the packet ({@code T}) at the head of this queue, if
+     * the queue is non-empty. If the queue is closed or empty, returns null
+     * without blocking.
+     * @return the packet at the head of this queue, or null if the queue is
+     * empty.
+     */
     T poll();
 
+    /**
+     * Closes current <tt>PacketQueue</tt> instance. No items will be added
+     * to queue when it's closed. Threads which were blocked in {@link #get()}
+     * will receive <tt>null</tt>. Asynchronous queue processing by
+     * {@link #asyncQueueHandler} is stopped.
+     */
     void close();
 
+    /**
+     * Gets a JSON representation of the parts of this object's state that
+     * are deemed useful for debugging.
+     */
     JSONObject getDebugState();
 
+    /**
+     * Sets the handler of errors (packets dropped or exceptions caught while
+     * processing).
+     * @param errorHandler the handler to set.
+     */
     void setErrorHandler(@NotNull ErrorHandler errorHandler);
 
     /**
