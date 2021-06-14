76558083817812f174a70b3c242a95dad6fd4671
James A <84336185+jqdrqgnq@users.noreply.github.com>
2021-06-14 11:42:48 -0400

    Fix json debug encoding of speaker levels. (#102)
    * Fix json debug encoding of speaker levels.
    
    Encode valid json.
    Encode array elements, not hashcode.

--- src/main/java/org/jitsi/utils/dsi/DominantSpeakerIdentification.java
+++ src/main/java/org/jitsi/utils/dsi/DominantSpeakerIdentification.java
@@ -1071,24 +1071,27 @@ public class DominantSpeakerIdentification<T>
          * Gets the (history of) audio levels received or measured for this
          * <tt>Speaker</tt>.
          *
-         * @return a <tt>byte</tt> array which represents the (history of) audio
+         * @return a <tt>String</tt> that lists the (history of) audio
          * levels received or measured for this <tt>Speaker</tt>
          */
-        byte[] getLevels()
+        String getLevels()
         {
             // The levels of Speaker are internally maintained starting with the
             // last audio level received or measured for this Speaker and ending
             // with the first audio level received or measured for this Speaker.
-            // Unfortunately, the method is expected to return levels in reverse
-            // order.
+            // Reverse the list and print them in the order they were received.
             byte[] src = this.levels;
-            byte[] dst = new byte[src.length];
+            StringBuffer sb = new StringBuffer();
+            sb.append('[');
 
-            for (int s = src.length - 1, d = 0; d < dst.length; --s, ++d)
+            for (int s = src.length - 1; s >= 0; --s)
             {
-                dst[d] = src[s];
+                sb.append(src[s]);
+                sb.append(',');
             }
-            return dst;
+
+            sb.setCharAt(sb.length() - 1, ']'); /* replace last comma */
+            return sb.toString();
         }
 
         /**
