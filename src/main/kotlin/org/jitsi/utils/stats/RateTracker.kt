f60ac1569e2dcb407b16b72b21e903bf8c47b24d
bgrozev <boris@jitsi.org>
2021-10-27 12:14:15 -0700

    Fix slow initial convergence in RateTracker.  (#111)
    * fix: Fix the scale in RateStatistics.
    
    * fix: Slow initial convergence in RateTracker
    
    We used to use the full window as the denumerator when calculating the average rate.
    This results in slow convergence at the start, e.g. a tracker with a 5 s
    window would return only 20% of the observed rate when queried 1 s after
    a value was added.
    
    We now keep track of how much of the window is used, so we have a more accurate rate earlier.
    
    * test: Add a trace of packet sizes and a disabled RateTracker test that reads it.

--- src/main/kotlin/org/jitsi/utils/stats/RateTracker.kt
+++ src/main/kotlin/org/jitsi/utils/stats/RateTracker.kt
@@ -19,6 +19,7 @@ import org.jitsi.utils.ms
 import java.lang.IllegalArgumentException
 import java.time.Clock
 import java.time.Duration
+import kotlin.math.roundToLong
 
 /**
  * Tracks an average rate (of values added via [update]) over a sliding window. The data is kept in a circular buffer
@@ -81,6 +82,11 @@ open class RateTracker @JvmOverloads constructor(
     private var oldestTime: Long = 0
 
     /**
+     * The [clock] time at which the first value was inserted (via [update]).
+     */
+    private var firstInsertTimeMs = -1L
+
+    /**
      * The size of the window in milliseconds.
      */
     private val windowSizeMs = numBuckets * bucketSize.toMillis()
@@ -119,6 +125,10 @@ open class RateTracker @JvmOverloads constructor(
             }
         }
         oldestTime = newOldestTime
+        if (accumulatedCount == 0L) {
+            // The stream has had no packets in a whole window, reset.
+            firstInsertTimeMs = -1
+        }
     }
 
     /**
@@ -127,7 +137,10 @@ open class RateTracker @JvmOverloads constructor(
     @Synchronized
     open fun getRate(nowMs: Long): Long {
         eraseOld(coerceMs(nowMs))
-        return (accumulatedCount.toDouble() * 1000 / windowSizeMs + 0.5f).toLong()
+
+        // This avoids slow convergence at the start if the full window is used to compute the average rate.
+        val effectiveWindowSizeMs = (nowMs - firstInsertTimeMs).coerceIn(bucketSizeMs, windowSizeMs)
+        return (accumulatedCount.toDouble() * 1000 / effectiveWindowSizeMs).roundToLong()
     }
     val rate: Long
         get() = getRate(clock.millis())
@@ -145,6 +158,7 @@ open class RateTracker @JvmOverloads constructor(
         val now = coerceMs(nowMs)
         if (now < oldestTime) // Too old data is ignored.
             return
+        if (firstInsertTimeMs < 0) firstInsertTimeMs = nowMs
         eraseOld(now)
         val nowOffset = (now - oldestTime).toInt()
         var index = oldestIndex + nowOffset
@@ -164,11 +178,11 @@ open class RateStatistics @JvmOverloads constructor(
 ) {
 
     val tracker = RateTracker(windowSizeMs, 1.ms, clock)
-    val scale = scale / (windowSizeMs - 1)
+    val scale = scale / windowSizeMs
 
     val rate: Long
         get() = getRate(clock.millis())
-    open fun getRate(nowMs: Long = clock.millis()) = (tracker.getRate(nowMs) * scale + 0.5).toLong()
+    open fun getRate(nowMs: Long = clock.millis()) = (tracker.getRate(nowMs) * scale).roundToLong()
 
     @JvmOverloads
     fun getAccumulatedCount(nowMs: Long = clock.millis()) = tracker.getAccumulatedCount(nowMs)
